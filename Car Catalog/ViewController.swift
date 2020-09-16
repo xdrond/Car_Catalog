bc2ec91442da27b0ee89e61e8e9fa2e7c96b2ab2
xdrond <xdrond@gmail.com>
2020-09-16 19:18:45 +0700

    Initial Commit

---
+++ Car Catalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car Catalog
+//
+//  Created by xdrond on 16.09.2020.
+//  Copyright © 2020 romanromanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
