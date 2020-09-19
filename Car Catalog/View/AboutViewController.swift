f62b1843d5b5bdb8c9a5f5e4a76cd36d8913d6f9
xdrond <xdrond@gmail.com>
2020-09-19 22:12:22 +0700

    Two screens were created.
    1) CarsTableViewController - for the list of all cars, AboutViewController - for viewing and editing the car properties.
    2) NavigationController is connected.

---
+++ Car Catalog/View/AboutViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.swift
+//  Car Catalog
+//
+//  Created by xdrond on 19.09.2020.
+//  Copyright © 2020 romanromanov. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+}
--- Car Catalog/View/CarsTableViewController.swift
+++ Car Catalog/View/CarsTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CarsTableViewController: UITableViewController {
 
     var modelController: ModelController!
 
@@ -17,11 +17,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         modelController = ModelController()
-        cars = try? modelController.retrieveAllCars()!
+
 
         // Do any additional setup after loading the view.
     }
-
 
 }
 
